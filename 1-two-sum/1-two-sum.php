89bfbcc4057a163ca3a04bd148bd05618f6bd509
Misbahur Rashid <67305300+Misbahur-Rashid@users.noreply.github.com>
2022-06-14 22:42:37 +0600

    Time: 3202 ms (5.02%), Space: 20 MB (53.55%) - LeetHub

---
+++ 1-two-sum/1-two-sum.php
@@ -0,0 +1,26 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        $sum = 0;
+        $number1 = 0;
+        $number2 = 0;
+        for($i=0; $i<count($nums); $i++){
+            $number1 = $nums[$i];
+            for($j=0; $j<count($nums); $j++){
+                if($i == $j){
+                    continue;
+                }
+                $number2 = $nums[$j];
+                if(($number1 + $number2) == $target){
+                    break 2;
+                }
+            }
+        }
+        return[$i, $j];
+    }
+}
