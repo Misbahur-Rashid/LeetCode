f72f14af9d7fa62548d172e2e237307fd50cd41e
Misbahur Rashid <67305300+Misbahur-Rashid@users.noreply.github.com>
2022-09-11 08:12:04 +0600

    Time: 202 ms (18.31%), Space: 36.8 MB (74.47%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -1,22 +1,45 @@
+// class Solution {
+// public:
+//     int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
+//         vector<pair<int, int>> ord;
+//         for (int i = 0; i < n; i++)
+//             ord.push_back({efficiency[i], speed[i]});
+//         sort(ord.rbegin(), ord.rend());
+//         priority_queue<int> sppq;
+//         long totalSpeed = 0, best = 0;
+//         for (auto& p : ord) {
+//             int spd = p.second;
+//             sppq.push(-spd);
+//             if (sppq.size() <= k) totalSpeed += spd;
+//             else {
+//                 totalSpeed += spd + sppq.top();
+//                 sppq.pop();
+//             }
+//             best = max(best, totalSpeed * p.first);
+//         }
+//         return best % 1000000007;
+//     }
+// };
+
 class Solution {
 public:
-    int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
-        vector<pair<int, int>> ord;
-        for (int i = 0; i < n; i++)
-            ord.push_back({efficiency[i], speed[i]});
-        sort(ord.rbegin(), ord.rend());
-        priority_queue<int> sppq;
-        long totalSpeed = 0, best = 0;
-        for (auto& p : ord) {
-            int spd = p.second;
-            sppq.push(-spd);
-            if (sppq.size() <= k) totalSpeed += spd;
-            else {
-                totalSpeed += spd + sppq.top();
-                sppq.pop();
-            }
-            best = max(best, totalSpeed * p.first);
-        }
-        return best % 1000000007;
+  int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
+    vector<pair<int, int>> es;
+    for (int i = 0; i < n; ++i)
+      es.push_back({efficiency[i], speed[i]});
+    sort(rbegin(es), rend(es));
+    priority_queue<int, vector<int>, greater<int>> q;
+    long sum = 0;
+    long ans = 0;
+    for (int i = 0; i < n; ++i) {
+      if (i >= k) {
+        sum -= q.top();
+        q.pop();
+      }
+      sum += es[i].second;
+      q.push(es[i].second);
+      ans = max(ans, sum * es[i].first);
     }
+    return ans % static_cast<int>(1e9 + 7);
+  }
 };
