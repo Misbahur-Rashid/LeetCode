76878cd935c4df0cf48d53e5094dd42ed0b4e54f
Misbahur Rashid <67305300+Misbahur-Rashid@users.noreply.github.com>
2022-09-18 08:14:23 +0600

    Time: 7 ms (88.76%), Space: 14.8 MB (81.31%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -8,42 +8,58 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+//         if(l1 == NULL) return l2;
+//         if(l2 == NULL) return l1;
+//         //let the list with smaller head be cur1
+//         ListNode *h, *cur1, *cur2;
+//         if(l1->val <= l2->val){
+//             h = cur1 = l1;
+//             cur2 = l2;
+//         }else{
+//             h = cur1 = l2;
+//             cur2 = l1;
+//         }
+//         if(cur1->next == NULL){
+//             cur1->next = cur2;
+//             return cur1;
+//         }
+//         //stop when one of the list ends
+//         while(cur2 != NULL && cur1 != NULL){
+//             // cout << cur1->val << " " << cur2->val << endl;
+//             if(cur1->next == NULL){
+//                 cur1->next = cur2;
+//                 //done processing first list
+//                 cur1 = NULL;
+//             }else if(cur2->val <= cur1->next->val){
+//                 //merge cur2 into l1 and move cur2
+//                 ListNode* tmp = cur1->next;
+//                 cur1->next = cur2;
+//                 cur2 = cur2->next;
+//                 cur1->next->next = tmp;
+//             }else{
+//                 //move cur1 to the next
+//                 cur1 = cur1->next;
+//             }
+//         }
+//         return h;
+//     }
+// };
+
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(l1 == NULL) return l2;
-        if(l2 == NULL) return l1;
-        //let the list with smaller head be cur1
-        ListNode *h, *cur1, *cur2;
+        if(l1 == nullptr) return l2;
+        if(l2 == nullptr) return l1;
         if(l1->val <= l2->val){
-            h = cur1 = l1;
-            cur2 = l2;
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
         }else{
-            h = cur1 = l2;
-            cur2 = l1;
+            l2->next = mergeTwoLists(l2->next, l1);
+            return l2;
         }
-        if(cur1->next == NULL){
-            cur1->next = cur2;
-            return cur1;
-        }
-        //stop when one of the list ends
-        while(cur2 != NULL && cur1 != NULL){
-            // cout << cur1->val << " " << cur2->val << endl;
-            if(cur1->next == NULL){
-                cur1->next = cur2;
-                //done processing first list
-                cur1 = NULL;
-            }else if(cur2->val <= cur1->next->val){
-                //merge cur2 into l1 and move cur2
-                ListNode* tmp = cur1->next;
-                cur1->next = cur2;
-                cur2 = cur2->next;
-                cur1->next->next = tmp;
-            }else{
-                //move cur1 to the next
-                cur1 = cur1->next;
-            }
-        }
-        return h;
     }
 };
