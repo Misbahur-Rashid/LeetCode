aa165f80f202d1020b506772cbe86464bbe3475e
Misbahur Rashid <67305300+Misbahur-Rashid@users.noreply.github.com>
2022-09-23 09:06:16 +0600

    Time: 58 ms (72.83%), Space: 5.9 MB (82.61%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -1,13 +1,26 @@
+// class Solution {
+// public:
+//     int concatenatedBinary(int n) {
+//         long res=0;
+//         int i,mod=1000000007;
+//         for(i=1;i<=n;i++)
+//         {
+//             int bits=(int)(log(i)/log(2)+1);
+//             res=((res<<bits)|i)%mod;
+//         }
+//         return (int)res;
+//     }   
+// };
+
 class Solution {
 public:
-    int concatenatedBinary(int n) {
-        long res=0;
-        int i,mod=1000000007;
-        for(i=1;i<=n;i++)
-        {
-            int bits=(int)(log(i)/log(2)+1);
-            res=((res<<bits)|i)%mod;
-        }
-        return (int)res;
-    }   
+  int concatenatedBinary(int n) {
+    constexpr int kMod = 1e9 + 7;
+    long ans = 0;    
+    for (int i = 1, len = 0; i <= n; ++i) {
+      if ((i & (i - 1)) == 0) ++len;
+      ans = ((ans << len) % kMod + i) % kMod;
+    }
+    return ans;
+  }
 };
