d9d1d3e20bea97a00b619ccc7abfbf0f84a80448
Misbahur Rashid <67305300+Misbahur-Rashid@users.noreply.github.com>
2022-09-24 06:31:10 +0600

    Time: 20 ms (54.55%), Space: 19.7 MB (98.57%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -7,31 +7,74 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+// class Solution {
+// public:
+//     vector<vector<int>> pathSum(TreeNode* root, int sum) {
+//         vector<vector<int>> res;
+//         vector <int> path;
+//         DFS(path, root, sum, res);
+//         return res;
+//     }
+    
+//     void DFS(vector <int> path, TreeNode* root, int sum, vector<vector<int>> &res)
+//     {
+//         if (root == NULL)
+//             return;
+//         if (root->left == NULL && root->right == NULL)
+//         {
+//             if (root->val == sum)
+//             {
+//                 path.push_back(root->val);
+//                 res.push_back(path);
+//                 path.pop_back();
+//             }
+//         }
+//         path.push_back(root->val);
+//         DFS(path, root->left, sum - root->val, res);
+//         DFS(path, root->right, sum - root->val, res);
+//         path.pop_back();
+//     }
+// };
+
+
+
 class Solution {
 public:
-    vector<vector<int>> pathSum(TreeNode* root, int sum) {
-        vector<vector<int>> res;
-        vector <int> path;
-        DFS(path, root, sum, res);
-        return res;
-    }
+    vector<vector<int>> ans;
+    int sum;
     
-    void DFS(vector <int> path, TreeNode* root, int sum, vector<vector<int>> &res)
-    {
-        if (root == NULL)
-            return;
-        if (root->left == NULL && root->right == NULL)
-        {
-            if (root->val == sum)
-            {
-                path.push_back(root->val);
-                res.push_back(path);
+    void backtrack(TreeNode* cur, vector<int>& path){
+        //cur must not be nullptr
+        //when entering this function, cur is added in "path"
+        if(!cur->left && !cur->right){
+            // for(const int& e : path) cout << e << " ";
+            // cout << endl;
+            if(accumulate(path.begin(), path.end(), 0) == sum){
+                ans.push_back(path);
+            }
+        }else{
+            if(cur->left){
+                path.push_back(cur->left->val);
+                backtrack(cur->left, path);
+                path.pop_back();
+            }
+            
+            if(cur->right){
+                path.push_back(cur->right->val);
+                backtrack(cur->right, path);
                 path.pop_back();
             }
         }
-        path.push_back(root->val);
-        DFS(path, root->left, sum - root->val, res);
-        DFS(path, root->right, sum - root->val, res);
-        path.pop_back();
+    }
+    
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        if(!root) return vector<vector<int>>();
+        
+        this->sum = sum;
+        
+        vector<int> path = {root->val};
+        backtrack(root, path);
+        
+        return ans;
     }
 };
