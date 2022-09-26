d9f863063d63338fc277ea078d476eff04c71631
Misbahur Rashid <67305300+Misbahur-Rashid@users.noreply.github.com>
2022-09-26 08:52:21 +0600

    Time: 11 ms (53.23%), Space: 11.5 MB (33.76%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -25,6 +25,7 @@ public:
         for (string s : equations) {
             if (s[1] == '=') continue;
             //cout<<M[s[0]]<<M[s[3]]<<endl;
+            
             if (s[0] == s[3] or M.find(s[0]) != M.end() and M.find(s[3]) != M.end() and M[s[0]] == M[s[3]]) return false;
         }
         return true;
