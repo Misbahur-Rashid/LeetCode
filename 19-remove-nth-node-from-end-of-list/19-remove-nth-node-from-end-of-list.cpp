2352305e32e63e81c395c20814f984541de804dc
Misbahur Rashid <67305300+Misbahur-Rashid@users.noreply.github.com>
2022-09-28 10:02:32 +0600

    Time: 3 ms (91.83%), Space: 10.7 MB (75.00%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -8,36 +8,71 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         ListNode* fast;
+//         ListNode* slow;
+
+//         fast = head;
+//         slow = head;
+
+//         while(n){
+//             fast = fast->next;
+//             n--;
+//         }
+
+//         if(fast == NULL){
+//             head = head->next;
+//             return head;
+//         }
+
+//         while(fast->next){
+//             slow = slow->next;
+//             fast = fast->next;
+//         }
+
+//         if(slow->next && slow->next->next){
+//             slow->next = slow->next->next;
+//         } else {
+//             slow->next = NULL;
+//         }
+
+//         return head;
+//     }
+// };
+
+
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* fast;
-        ListNode* slow;
-
-        fast = head;
-        slow = head;
-
-        while(n){
-            fast = fast->next;
-            n--;
+        ListNode* dummy = new ListNode();
+        dummy->next = head;
+        
+        ListNode* cur;
+        int len = 0;
+        
+        cur = dummy->next;
+        while(cur){
+            ++len;
+            cur = cur->next;
         }
-
-        if(fast == NULL){
-            head = head->next;
-            return head;
+        
+        // cout << "len: " << len << endl;
+        
+        cur = dummy;
+        for(int i = 0; i < len; ++i, cur = cur->next){
+            //i is 1-based
+            // cout << i << " : " << cur->val << endl;
+            if(i == len-n){
+                //this is the node previous to the node to be deleted
+                ListNode* tmp = cur->next;
+                cur->next = cur->next->next;
+                delete tmp;
+                break;
+            }
         }
-
-        while(fast->next){
-            slow = slow->next;
-            fast = fast->next;
-        }
-
-        if(slow->next && slow->next->next){
-            slow->next = slow->next->next;
-        } else {
-            slow->next = NULL;
-        }
-
-        return head;
+        
+        return dummy->next;
     }
 };
