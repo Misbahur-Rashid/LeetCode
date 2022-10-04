15acb6c95cdba7d64a63b77500897defd8a7b9ea
Misbahur Rashid <67305300+Misbahur-Rashid@users.noreply.github.com>
2022-10-04 20:22:54 +0600

    Time: 80 ms (78.94%), Space: 31.1 MB (92.72%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,56 +1,56 @@
-// class Solution {
-// public:
-//     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-//         auto it = lower_bound(arr.begin(), arr.end(), x);
-        
-//         if(it == arr.begin()){
-//             return vector<int>(arr.begin(), arr.begin()+k);
-//         }else if(it == arr.end()){
-//             return vector<int>(arr.end() - k, arr.end());
-//         }
-        
-//         int n = arr.size();
-//         int idx = it - arr.begin();
-//         int left, right;
-//         left = idx - k/2;
-//         right = left + k;
-                
-//         if(left < 0){
-//             left = 0;
-//             right = left +k;
-//         }
-        
-//         if(right > n){
-//             right = n;
-//             left = right - k;
-//         }
-        
-//         while(left > 0 && x - arr[left-1] <= arr[right-1] - x){
-//             right--;
-//             left--;
-//         }
-//         while(right < n && arr[(right-1)+1] - x < x - arr[left]){
-//             right++;
-//             left++;
-//         }
-                
-//         return vector<int>(arr.begin()+left , arr.begin()+right);
-//     }
-// };
-
-
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        sort(arr.begin(), arr.end(), [x](const int& a, const int& b){
-            return (abs(a-x) == abs(b-x)) ? a < b : (abs(a-x) < abs(b-x));
-        });
+        auto it = lower_bound(arr.begin(), arr.end(), x);
         
-        vector<int> ans(arr.begin(), arr.begin()+k);
-        //now ans is not ascending, need to sort it again
+        if(it == arr.begin()){
+            return vector<int>(arr.begin(), arr.begin()+k);
+        }else if(it == arr.end()){
+            return vector<int>(arr.end() - k, arr.end());
+        }
         
-        sort(ans.begin(), ans.end());
+        int n = arr.size();
+        int idx = it - arr.begin();
+        int left, right;
+        left = idx - k/2;
+        right = left + k;
+                
+        if(left < 0){
+            left = 0;
+            right = left +k;
+        }
         
-        return ans;
+        if(right > n){
+            right = n;
+            left = right - k;
+        }
+        
+        while(left > 0 && x - arr[left-1] <= arr[right-1] - x){
+            right--;
+            left--;
+        }
+        while(right < n && arr[(right-1)+1] - x < x - arr[left]){
+            right++;
+            left++;
+        }
+                
+        return vector<int>(arr.begin()+left , arr.begin()+right);
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+//         sort(arr.begin(), arr.end(), [x](const int& a, const int& b){
+//             return (abs(a-x) == abs(b-x)) ? a < b : (abs(a-x) < abs(b-x));
+//         });
+        
+//         vector<int> ans(arr.begin(), arr.begin()+k);
+//         //now ans is not ascending, need to sort it again
+        
+//         sort(ans.begin(), ans.end());
+        
+//         return ans;
+//     }
+// };
